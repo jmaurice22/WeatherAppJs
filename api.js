b5aaa5676c10d0159f90cdcb5d75c38910c677d3
jmaurice22 <jpostell29@gmail.com>
2021-07-17 01:18:57 -0700

    Fixed api key & Resp Obj

--- api.js
+++ api.js
@@ -1,36 +1,26 @@
-/*Get the input element */
-var searchBar = document.getElementById("searchBar");
-var searchValue = searchBar.value;
-
-const weather = {};
+/*
+API Key = "48b1d0ac40664100a210c1d87ff1f450"
+PROVIDER = WEATHERBIT.IO
+*/
 
 /* Button Element*/
 var button = document.getElementById("searchButton");
-button.onclick = getWeather;
+button.addEventListener("click", getWeather);
 
 
 /* Callback function to handle search event */
 
-//  TODO: FIX API KEY ISSUES AND GET RESPONSE OBJ DATA
-const key = 'c911883c686ef69e6975099842481ee7';
-function getWeather() {
-  // API Key
-  let url = `http://api.openweathermap.org/data/2.5/weather?q={searchValue}&appid={key}`;
-
-  //API Call
-  fetch(url)
-    .then(function (response) {
-      let data = response.json();
-      return data;
-    })
-    // .then(function (data) {
-    //   weather.temperature.value = Math.floor(data.main.temp - KELVIN);
-    //   weather.description = data.weather[0].description;
-    //   weather.iconId = data.weather[0].icon;
-    //   weather.city = data.name;
-    //   weather.country = data.sys.country;
-    // })
-    .then(function () {
-      console.log("success");
-    });
+//  TODO: EXTRACT RESPONSE OBJ DATA 
+//  AND DISPLAY IT TO THE DOM
+async function getWeather() {
+   /*Get the input element AND VALUE*/
+  let searchBar = document.getElementById("searchBar");
+  let searchValue = searchBar.value;
+  // URL
+  let url = `https://api.weatherbit.io/v2.0/current?postal_code=${searchValue}&country=US&units=I&key=48b1d0ac40664100a210c1d87ff1f450`;
+  // API Call
+  let response = await fetch(url);
+  // CHANGE THE RESPONSE OBJ TO JSON   
+  let data = await response.json();
+  console.log(data);
 }
