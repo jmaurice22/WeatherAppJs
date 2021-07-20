d4adac3207bf9caee418b45a3649e860edb7c6a4
jmaurice22 <jpostell29@gmail.com>
2021-07-19 20:19:29 -0700

    added error handling

--- api.js
+++ api.js
@@ -22,24 +22,32 @@ async function getWeather(e) {
   // URL
   let url = `https://api.weatherbit.io/v2.0/current?city=${searchValue}&country=US&units=I&key=48b1d0ac40664100a210c1d87ff1f450`;
   // REQUEST
+  // Error handling
+
   let response = await fetch(url);
-  // CHANGE THE RESPONSE OBJ TO JSON   
-  let data = await response.json();
-  // EXTRACT DATA FROM RES OBJ
-  let cityName = data.data[0]['city_name'];
-  let temperature = data.data[0]['temp'];
-  let feel = data.data[0]['app_temp'];
-  let windSpeed = data.data[0]['wind_spd'];
-  let precip = data.data[0]['precip'];
-  
-  // POST VALUE TO THE DOM
-  loc = document.getElementById('city').textContent = cityName;
-  tempEl = document.getElementById('temperature').textContent = temperature + 'F';
-  feelEL = document.getElementById('feelsLike').textContent = feel;
-  wind = document.getElementById('maxTemp').textContent = windSpeed;
-  precip = document.getElementById('minTemp').textContent = precip;
-  
+  if (response.status === '"success' ){
+
+    // CHANGE THE RESPONSE OBJ TO JSON   
+    let data = await response.json();
+    // EXTRACT DATA FROM RES OBJ
+    let cityName = data.data[0]['city_name'];
+    let temperature = data.data[0]['temp'];
+    let feel = data.data[0]['app_temp'];
+    let windSpeed = data.data[0]['wind_spd'];
+    let precip = data.data[0]['precip'];
+    // POST VALUE TO THE DOM
+    loc = document.getElementById('city').textContent = cityName;
+    tempEl = document.getElementById('temperature').textContent = temperature + 'F';
+    feelEL = document.getElementById('feelsLike').textContent = feel;
+    wind = document.getElementById('maxTemp').textContent = windSpeed;
+    precip = document.getElementById('minTemp').textContent = precip;
+  } 
+  else  {
+    alert("Could not complete request");
+  }
   reset();
+  
+  
 }
 
 function reset() {
